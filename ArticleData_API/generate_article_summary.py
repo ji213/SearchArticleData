a5e8eb42738f5086a4d46e1baa279b44030c7904
ji213 <ittyjonathan@gmail.com>
2026-02-06 17:44:17 -0500

    added logic to successfully scape web data... still debugging effectiveness at this point

--- ArticleData_API/generate_article_summary.py
+++ ArticleData_API/generate_article_summary.py
@@ -1,4 +1,6 @@
 from db_config import get_db_connection_string
+import trafilatura
+from playwright.sync_api import sync_playwright
 import pyodbc
 
 def get_article_to_summarize():
@@ -11,7 +13,7 @@ def get_article_to_summarize():
         with pyodbc.connect(connection_string) as conn:
             cursor = conn.cursor()
             print("Querying Database...")
-            cursor.execute("{CALL dbo.usp_GetUnsummarizedArticle}")
+            cursor.execute("{CALL dbo.usp_GetUnsummarizedArticle_atRandom}")
             row = cursor.fetchone()
 
             if row:
@@ -29,6 +31,51 @@ def get_article_to_summarize():
     except Exception as e:
         print(f"❌ Error fetching: {e}")
     return article_data
+
+def get_article_text (url):
+    # inputs the article URL and gathers the text from it.
+
+    try:
+        print(f"🚀 Launching browser to bypass protections...")
+
+        with sync_playwright() as p:
+            # Launch browser
+            browser = p.chromium.launch(headless=True)
+
+            # Mimic a real Windows Chrome user
+            context = browser.new_context(
+                user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"
+            )
+
+            page = context.new_page()
+
+            #Navigate and wait for the page to stop loading data
+            # wait_until="dotcomloaded" -  waits for the basic HTML has been completely downloaded and parsed into the DOM
+            print(f"🌐 Navigating to: {url}")
+            page.goto(url, wait_until="domcontentloaded", timeout=60000)
+
+            #SCREENSHOT DEBUG
+            page.screenshot(path="debug.png")
+            print("📸 Debug screenshot saved as debug_view.png")
+
+            # wait for 3 seconds for any final JS/popups to clear
+            page.wait_for_timeout(3000)
+
+            #Grab the FULLY RENDERED HTML
+            rendered_html = page.content()
+            browser.close()
+
+            # Use Trafilatura to extract clean text from the rendered HTML
+            article_text = trafilatura.extract(rendered_html, favor_precision=True)
+
+            
+
+            # return article text, return none if article text doesnt exist
+            return article_text.strip() if article_text else None
+    except Exception as e:
+        print(f"❌ Scraping Error: {e}")
+        return None
+
 
 
 def main():
@@ -56,6 +103,19 @@ def main():
 
         print("-" * 40)
         print("\n Ready for next steps...")
+
+        # Test scraping logic
+        print("\n📝 TESTING... SCRAPING CONTENT...")
+        test_content = get_article_text(article['url'])
+
+        if test_content:
+            print(f"✅ Successfully scraped {len(test_content)} characters.")
+            print("-" * 40)
+            # Show just the first 300 characters to verify it's real text
+            print(test_content[:2000] + "...")
+            print("-" * 40)
+        else:
+            print("⚠️ Trafilatura returned no text.")
     else:
         print("\n No data available to display")
 
---
+++ SQLScripts/StoredProcedures/dbo.usp_GetUnsummarizedArticle_byID.StoredProcedure.sql
@@ -0,0 +1,16 @@
+CREATE OR ALTER PROCEDURE dbo.usp_GetUnsummarizedArticle_byID (
+    @ArticleID INT = NULL  -- Added optional input parameter
+)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    -- This is only for debugging purposes, so we will always pass in a parameter
+    
+    SELECT TOP 1 ArticleID, URL, Title, Domain, Language
+    FROM dbo.tbl_articles
+    WHERE GeneratedSummary IS NULL
+      -- If @ArticleID is provided, filter by it; otherwise, ignore this filter
+    AND ArticleID = @ArticleID
+    ORDER BY ArticleID ASC; 
+END
